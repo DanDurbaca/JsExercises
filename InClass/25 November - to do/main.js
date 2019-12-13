f1a7b0f1e9daf9f8c75cefd4c5549ecdf498e2a4
Dan Durbaca <DuD@ltpemdomain.local>
2019-12-13 15:00:07 +0100

    added animated expansion to chessboard

--- InClass/25 November - to do/main.js
+++ InClass/25 November - to do/main.js
@@ -1,13 +1,14 @@
+"use strict";
 
 function buildHTMLBoard(sizeOfBoard)
 {
-    var myHtmlItem = document.getElementById("container");
-    for (lines = 0; lines <sizeOfBoard;lines++)
+    let myHtmlItem = document.getElementById("container");
+    for (let lines = 0; lines <sizeOfBoard;lines++)
     {
-        newRow = document.createElement("div");
-        for(i=0;i<sizeOfBoard;i++)
+        let newRow = document.createElement("div");
+        for(let i=0;i<sizeOfBoard;i++)
         {
-            newElement = document.createElement("div");
+            let newElement = document.createElement("div");
             
             newElement.classList.add("box");
             newElement.classList.add("yellowBox");
@@ -21,6 +22,72 @@ function buildHTMLBoard(sizeOfBoard)
     }
 }
 
+
+function start_colouring(iPos,jPos)
+{
+
+}
+
+function myBoxWasClicked()
+{
+    let myElement = event.srcElement;
+    let bombsAround = 0;
+    if (!visitedBoard[myElement.address_line][myElement.address_column])
+    {
+        visitedBoard[myElement.address_line][myElement.address_column] = 1;
+
+        if (myElement.address_line-1>=0)
+            bombsAround+= board[myElement.address_line-1][myElement.address_column];
+
+        if ((myElement.address_line-1>=0) && (myElement.address_column-1>=0))
+            bombsAround+= board[myElement.address_line-1][myElement.address_column-1];
+
+        if ((myElement.address_line-1>=0) && (myElement.address_column+1<nSize))
+            bombsAround+= board[myElement.address_line-1][myElement.address_column+1];
+
+        if (myElement.address_column+1<nSize)
+            bombsAround+= board[myElement.address_line][myElement.address_column+1];
+
+        if (myElement.address_column-1>=0)
+            bombsAround+= board[myElement.address_line][myElement.address_column-1];
+
+        if (myElement.address_line+1<nSize)
+            bombsAround+= board[myElement.address_line+1][myElement.address_column];
+
+        if ((myElement.address_line+1<nSize)&&(myElement.address_column-1>=0))
+            bombsAround+= board[myElement.address_line+1][myElement.address_column-1];
+
+        if ((myElement.address_line+1<nSize)&&(myElement.address_column+1<nSize))
+            bombsAround+= board[myElement.address_line+1][myElement.address_column+1];
+
+        switch (bombsAround){
+            case 0: myElement.classList.add("greenBox");
+            break;
+            case 1: myElement.classList.add("blueBox");
+            break;
+            case 2: myElement.classList.add("orangeBox");
+            break;
+            case 3: myElement.classList.add("redBox");
+            break;
+        }
+
+        
+        // trigger around:
+        setTimeout(function() {triggerCell(myElement.address_line+1,myElement.address_column);}, 100);
+        setTimeout(function() {triggerCell(myElement.address_line-1,myElement.address_column);}, 100);
+        setTimeout(function() {triggerCell(myElement.address_line,myElement.address_column+1);}, 100);
+        setTimeout(function() {triggerCell(myElement.address_line,myElement.address_column-1);}, 100);
+
+    }
+}
+
+function triggerCell(i,j)
+{
+    let elem = document.getElementById("myBox_" + i + '-' + j);
+    elem.click();
+}
+
+/*
 function myBoxWasClicked()
 {
     var myElement = event.srcElement;
@@ -54,15 +121,15 @@ function myBoxWasClicked()
     //alert(event.srcElement.id);
     // check THIS 
     // https://stackoverflow.com/questions/4825295/javascript-onclick-to-get-the-id-of-the-clicked-button
-}
+}*/
 
 function generateBoard(sizeOfBoard)
 {
-    var localBoard = new Array(sizeOfBoard);
-    for (var i = 0; i < sizeOfBoard; i++) 
+    let localBoard = new Array(sizeOfBoard);
+    for (let i = 0; i < sizeOfBoard; i++) 
     {
         localBoard[i] = new Array(sizeOfBoard);
-        for (j = 0;j<sizeOfBoard;j++)
+        for (let j = 0;j<sizeOfBoard;j++)
         {
             localBoard[i][j] = 0;
         }
@@ -72,19 +139,25 @@ function generateBoard(sizeOfBoard)
 
 function addBombs(numBombs)
 {
-    for(i=0;i<numBombs;i++)
+    for(let i=0;i<numBombs;i++)
     {
-        newBombI = Math.floor(Math.random() * 10);
-        newBombJ = Math.floor(Math.random() * 10);
+        let newBombI = Math.floor(Math.random() * nSize);
+        let newBombJ = Math.floor(Math.random() * nSize);
         board[newBombI][newBombJ] = 1;
-        elem = document.getElementById("myBox_"+ newBombI + '-' + newBombJ);
-        elem.classList.add("bombBox");
+        //let elem = document.getElementById("myBox_"+ newBombI + '-' + newBombJ);
+        //elem.classList.add("bombBox");
     }
 }
 
-var nSize = 10;
+var nSize = 40;
 var board = generateBoard(nSize, 10);
+var visitedBoard = generateBoard(nSize, 10);
 buildHTMLBoard(nSize);
 
 
-console.log(board[2]);
+function testColorChange(elem)
+{
+    elem.classList.add("greenBox");
+}
+
+//console.log(board[2]);
