f2801ddbf92ef98497af73855da39850c11c9f55
Dan Durbaca <DuD@ltpemdomain.local>
2020-03-10 14:26:04 +0100

    color transitioning 10th of March

--- Semester 2/9th ofMarch/main.js
+++ Semester 2/9th ofMarch/main.js
@@ -1,6 +1,6 @@
 function ReadAndAlertRadio()
 {
-    //alert("we are ok");
+    /*//alert("we are ok");
     if (document.getElementById("male").checked == true)
     {
         alert(document.getElementById("male").value)
@@ -22,6 +22,37 @@ function ReadAndAlertRadio()
     {
         alert("nothing is selected");
     }
-    
+*/
+    let element = document.getElementById("whatever");
+    element.style.backgroundColor="rgb(155, 102, 102)"; 
+}
 
+
+function buildCSSColor(r,g,b)
+{
+    let retVal = "rgb(" + r + "," + g + "," + b +")";
+
+    return retVal;
+}
+
+//var boxIsChangingColors = false;
+
+function StartChangingColors()
+{
+    //let MyElement = document.getElementById("ColoredBox");
+    let MyElements = document.querySelectorAll(".bla");
+    let a = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    let c = Math.floor(Math.random()*255);
+    let myStringColor =  buildCSSColor(a,b,c);
+    console.log(myStringColor);
+
+    /*
+    for (let i = 0; i < MyElements.length; i++) {
+        MyElements[i].style.backgroundColor = myStringColor;
+    } */
+  
+    for (t of MyElements){
+        t.style.backgroundColor = myStringColor;
+    }
 }
